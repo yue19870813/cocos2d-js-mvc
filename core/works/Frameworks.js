330350715f567f32416963b6fa579e4b46d542bd
ituuz <yue19870813@163.com>
2015-06-29 20:42:24 +0800

    To optimize the code and add exegesis.

--- core/works/Frameworks.js
+++ core/works/Frameworks.js
@@ -1,12 +1,19 @@
+/********************************************************************************
+  The game Frameworks
+  @author ituuz 
+  @date 2014-11-24
+  
+  The framework's summary.
+********************************************************************************/
 
 var game = game || {};
 var server = server || {};
 
 game.Frameworks = {
 		DEBUG:false,
-		LOG_TAG:"DEBUG",	//日志tag
-		IS_RUNTIME:false,	//是否是runtime
-		NET_TYPE:1,		//网络类型
-		DESIGN_ZOOM:1,	//游戏适配缩放
-		DEVICE_ID:"DEVICE_ID"	//游戏适配缩放
+		LOG_TAG:"DEBUG",
+		IS_RUNTIME:false,	
+		NET_TYPE:1,		
+		DESIGN_ZOOM:1,	
+		DEVICE_ID:"DEVICE_ID"	
 }
