d5406d3612502acb8b9d3e105333c3b7610e125b
ituuz <yue19870813@163.com>
2015-06-29 20:38:38 +0800

    To optimize the code and add exegesis.

--- core/net/SocketManager.js
+++ core/net/SocketManager.js
@@ -1,134 +1,112 @@
-/******************************************
- * 消息协议管理类：
- * 		发送消息、注册消息等。
- * create by Yue 2014-11-13
- ******************************************/
-//单例
-//var SocketManager = (function(){
-//	game.log("SocketManager init.");
-//	var unique;
-//	unique = new _SocketManager();
-//	return unique;
-//})();
+/********************************************************************************
+  The game.SocketManager
+  @author ituuz 
+  @date 2014-11-13
+  
+  The manager of the protocol. It's can send message, register message, resend 
+  when net error and so on.
+********************************************************************************/
 
-var SocketManager = {};
-
-/** 
- * Public
- **/
-//连接地址
-SocketManager.address = "";
-
-//回调集合
-SocketManager.callbackMap = null;
-
-//session
-SocketManager.session = "";
+game.SocketManager = {};
 
 /** 
  * Private
  **/
-//是否初始化SocketManager
-SocketManager._isInit = false;
-//当前发送的对象，用于断线重连。
-SocketManager._obj = null;
-//服务器链接异常回调
-SocketManager._error = null;
+//The address of websocket
+game.SocketManager._address = "";
+//Callback list
+game.SocketManager._callbackMap = null;
+//User session
+game.SocketManager._session = "";
+//Is initialize SocketManager
+game.SocketManager._isInit = false;
+//Save the current send object used for resend when net error.
+game.SocketManager._obj = null;
+//Callback of the server error.
+game.SocketManager._error = null;
 
-//初始化SocketMananger
-SocketManager.init = function(addr, errorCallback) {
-	if (game.Frameworks.NET_TYPE == game.NetType.LOCAL) {
-		game.LocalStoreManager.init();
-		SocketManager.callbackMap = new game.Map();
-		SocketManager._isInit = true;
-	} else if (game.Frameworks.NET_TYPE == game.NetType.WS) {
-		SocketManager.address = addr;
-		SocketManager._error = errorCallback;
-		SocketManager.callbackMap = new game.Map();
-		SocketManager._isInit = true;
-	}
+//Initialize SocketMananger
+game.SocketManager.init = function(addr, errorCallback) {
+	game.SocketManager._address = addr;
+	game.SocketManager._error = errorCallback;
+	game.SocketManager._callbackMap = new game.Map();
+	game.ocketManager._isInit = true;
 }
 
-//注册服务器回调监听
-SocketManager.register = function (pid, callback) {
-	if (SocketManager._isInit == false) {
+//Register the server callback.
+game.SocketManager.register = function (pid, callback) {
+	if (game.SocketManager._isInit == false) {
 		game.log("SocketManager doesn't init!");
 		return;
 	}
-	SocketManager.callbackMap.put(pid, callback);
+	game.SocketManager._callbackMap.put(pid, callback);
 }
 
-//删除指定服务器监听
-SocketManager.unregister = function(pid) {
-	SocketManager.callbackMap.remove(pid);
+//Remove the specified protocol register. 
+game.SocketManager.unregister = function(pid) {
+	game.SocketManager._callbackMap.remove(pid);
 }
 
-//移除所有服务器监听
-SocketManager.clearRegister = function() {
-	SocketManager.callbackMap.clear();
+//Remove all the protocol register. 
+game.SocketManager.clearRegister = function() {
+	game.SocketManager._callbackMap.clear();
 }
 
-//发送协议
-SocketManager.send = function(obj) {
+//Send protocol message.
+game.SocketManager.send = function(obj) {
 
-	if (game.Frameworks.NET_TYPE == game.NetType.LOCAL) {
-		game.LocalStoreManager.requestHandler(obj);
-		return;
-	}
-	
-	if (SocketManager._isInit == false) {
+	if (game.SocketManager._isInit == false) {
 		game.log("SocketManager doesn't init!");
 		return;
 	}
-	obj.sid = SocketManager.session;
-	//保存当前消息用于断线重连
-	SocketManager._obj = obj;
+	obj.sid = game.SocketManager._session;
+	//Save the current send object used for resend when net error.
+	game.SocketManager._obj = obj;
 	
-	var ws = new WebSocket(SocketManager.address);
-	//创建连接成功监听
+	var ws = new WebSocket(game.SocketManager._address);
+	//Create net connect success.
 	ws.onopen = function(evt) {
 		var str = JSON.stringify(obj);
 		game.log("request text msg: " + str);
 		this.send(str);
 	};
 	
-	//连接错误
+	//Net error.
 	ws.onerror = function(evt) {
 		game.log("sendText Error was fired!");
-		if (SocketManager._error != null && SocketManager._error != undefined) {
-			SocketManager._error(evt);
+		if (game.SocketManager._error != null && game.SocketManager._error != undefined) {
+			game.SocketManager._error(evt);
 		}
 		this.close();
 	};
 	
-	//连接关闭
+	//Net close.
 	ws.onclose = function(evt) {
 		game.log("_wsiSendText websocket instance closed.");
 		this.close();
 	};
 	
-	//消息返回
+	//Server message back.
 	ws.onmessage = function(evt) {
 		game.log("response text msg: " + evt.data);
-		SocketManager.prase(evt.data);
+		game.SocketManager.prase(evt.data);
 		this.close();//关闭socket
 	};
 }
 
-//断线重连
-SocketManager.reSend = function() {
-	SocketManager.send(SocketManager._obj);
+//Resend message
+game.SocketManager.reSend = function() {
+	game.SocketManager.send(game.SocketManager._obj);
 }
 
-//解析返回协议
-SocketManager.prase = function(msg) {
-	game.log(msg, "MESSAGES");
+//Prase the server back message and send to register.
+game.SocketManager.prase = function(msg) {
 	var arr = JSON.parse(msg);
 	for (var i = 0; i < arr.length; i++) {
 		var pid = arr[i].pid;
-		var call = SocketManager.callbackMap.get(pid);
+		var call = game.SocketManager._callbackMap.get(pid);
 		if (call != undefined && call != null) {
-			call(arr[i]); //回调给注册点
+			call(arr[i]); //send to register
 		}
 	}
 }
